81f2f54f1a621097da92a34031df62f1c55de345
tazik <gor.prast@bk.ru>
2024-10-06 23:24:57 +0500

    feat: add realization of basic TelegramLongPollingBot methods

---
+++ src/main/java/org/example/TelegramBot.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotUsername() {
+        return System.getenv("BOT_NAME");
+    }
+
+    @Override
+    public String getBotToken() {
+        return System.getenv("BOT_TOKEN");
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText()){
+            Long chatId = update.getMessage().getChatId();
+            String message = update.getMessage().getText();
+        }
+    }
+
+}
