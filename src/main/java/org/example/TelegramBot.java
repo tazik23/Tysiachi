0e5715464f9367dc84adcc3db56ae3712848fd8f
tazik <gor.prast@bk.ru>
2024-10-06 23:27:37 +0500

    feat: add method to send message

--- src/main/java/org/example/TelegramBot.java
+++ src/main/java/org/example/TelegramBot.java
@@ -1,7 +1,9 @@
 package org.example;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class TelegramBot extends TelegramLongPollingBot {
 
@@ -20,6 +22,18 @@ public class TelegramBot extends TelegramLongPollingBot {
         if(update.hasMessage() && update.getMessage().hasText()){
             Long chatId = update.getMessage().getChatId();
             String message = update.getMessage().getText();
+        }
+    }
+
+    private void sendMessage(long chatID, String messageToSend){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(String.valueOf(chatID));
+        sendMessage.setText(messageToSend);
+        try {
+            execute(sendMessage);
+        }
+        catch (TelegramApiException e) {
+            e.printStackTrace();
         }
     }
 
